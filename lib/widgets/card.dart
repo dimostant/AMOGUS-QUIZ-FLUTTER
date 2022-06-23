f316d0da420fc8bbe5d5669f1dc9e4add4608b63
dimosthenes <ece01611@g.uowm.gr>
2022-06-23 04:04:11 +0300

    added timer, changed quiz Ui, changed main page Ui, added logout, Audio Class, Form, Consistency, Functionalities complete. Logout Button, package upgrades and uninstall

--- lib/widgets/card.dart
+++ lib/widgets/card.dart
@@ -1,15 +1,10 @@
 import 'package:flutter/material.dart';
+import '../classes/ScoreBoardContext.dart';
 
-class ScoreCard extends StatefulWidget {
-  const ScoreCard({Key? key}) : super(key: key);
+class ScoreCard extends StatelessWidget {
+  final ScoreBoardContext scoreBoardContext;
 
-  @override
-  State<ScoreCard> createState() => _ScoreCardState();
-}
-
-class _ScoreCardState extends State<ScoreCard> {
-
-  ScoreBoardContext scoreBoardContext = ScoreBoardContext(score: 5, date: DateTime.now().toString());
+  ScoreCard(this.scoreBoardContext);
 
   @override
   Widget build(BuildContext context) {
@@ -17,50 +12,32 @@ class _ScoreCardState extends State<ScoreCard> {
       color: Colors.redAccent,
       margin: EdgeInsets.fromLTRB(12, 10, 12, 10),
       child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children : [
-            Container(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Container(
               height: 60,
               padding: const EdgeInsets.all(12.0),
-              child: Column(
-
-                children: [
-                  Text('Score'),
-                  Text(scoreBoardContext.score.toString()),
-                ]
-              )
-            ),
-            Container(height: 60,
+              child: Column(children: [
+                Text('Score'),
+                Text(scoreBoardContext.score.toString()),
+              ])),
+          Container(
+              height: 60,
               padding: const EdgeInsets.all(12.0),
-              child: Column(
-                  children: [
-                    Text('Time'),
-                    Text(scoreBoardContext.date.toString().substring(11,19)),
-                  ]
-              )
-            ),
-            Container(
-                height: 60,
+              child: Column(children: [
+                Text('Time'),
+                Text(scoreBoardContext.time.toString() +
+                    ' s'), //.substring(11,19)),
+              ])),
+          Container(
+              height: 60,
               padding: const EdgeInsets.all(12.0),
-              child: Column(
-                  children: [
-                    Text('Date'),
-                    Text(scoreBoardContext.date.toString().substring(0,10)),
-                  ]
-              )
-            ),
-          ],
+              child: Column(children: [
+                Text('Date'),
+                Text(scoreBoardContext.date.toString()), //.substring(0,10)),
+              ])),
+        ],
       ),
     );
   }
-}
-
-
-class ScoreBoardContext {
-
-  int? score;
-  String? date;
-
-  ScoreBoardContext({this.score, this.date});
-
 }
